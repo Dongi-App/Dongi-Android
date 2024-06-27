06f78ac26f4b68ac459f508b2c17641343a61f1b
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-06-27 16:30:47 +0330

    chore: add server ip

--- app/src/main/java/com/example/dongi/api/RetrofitClient.kt
+++ app/src/main/java/com/example/dongi/api/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/" // For emulator
+    private const val BASE_URL = "http://91.228.186.176/" // For emulator
 
     private val gson = GsonBuilder()
         .setLenient()
